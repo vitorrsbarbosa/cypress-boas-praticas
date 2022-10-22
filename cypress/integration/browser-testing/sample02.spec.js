0e1d7c60fe8f37537ca76ec71a966304b28d34be
Vitor <vitorricardosilvabarbosa@gmail.com>
2022-10-22 13:20:33 -0300

    Bad habit 01 - browsertesting

---
+++ cypress/integration/browser-testing/sample02.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+describe('Browser testing bad practice - Second sample', () => {
+  beforeEach(() => {
+    cy.visit('https://hackernews-seven.vercel.app')
+  })
+
+  it('tests a browser feature instead of an app feature', () => {
+    cy.get('.table-row a')
+      // .first()
+      // .click()
+      .should('have.attr', 'href', 'https://99percentinvisible.org/episode/guerrilla-public-service/')
+      .should('have.attr', 'target', '_blank')
+    // Assert that a new tab was opened in the correct URL
+  })
+})
