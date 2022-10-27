e0947eb41cd0297f40863e864a88304e56c6483e
Vitor <vitorricardosilvabarbosa@gmail.com>
2022-10-27 15:32:45 -0300

    Bad habit 04 - Hardcoded assertions part 2

--- cypress/e2e/hardcoded-assertion/sample.cy.js
+++ cypress/e2e/hardcoded-assertion/sample.cy.js
@@ -11,17 +11,18 @@ describe('Hardcoded assertion bad practice', () => {
   });
 
   it('searches', () => {
+    const { hits } = require('../../fixtures/stories');
+
     cy.search('cypress.io');
     cy.wait('@getStories');
 
     cy.get('.table-row')
       .as('tableRows')
-      .should('have.length', 2);
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing');
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code');
+      .should('have.length', hits.length);
+    hits.forEach((hit, index) => {
+      cy.get('@tableRows')
+        .eq(index)
+        .should('contain', hit.title);
+    });
   });
 });
