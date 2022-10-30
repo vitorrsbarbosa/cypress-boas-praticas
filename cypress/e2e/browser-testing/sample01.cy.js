5a8b441cb1e828746c4363daae1867523e302240
Vitor <vitorricardosilvabarbosa@gmail.com>
2022-10-29 23:11:09 -0300

    Fix environment key

--- cypress/e2e/browser-testing/sample01.cy.js
+++ cypress/e2e/browser-testing/sample01.cy.js
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 describe('Browser testing bad practices - First sample', () => {
   beforeEach(() => {
-    cy.visit(Cypress.env('baseUrl'));
+    cy.visit(Cypress.env('base_url'));
   });
   it('directs the user to the login page when clicking the login link', () => {
     cy.contains('.nav a', 'Login').as('login')
       .should('have.attr', 'href', '/login')
       .should('not.have.attr', 'target');
     cy.get('@login').click();
-    cy.url().should('be.equal', `${Cypress.env('baseUrl')}/login`);
+    cy.url().should('be.equal', `${Cypress.env('base_url')}/login`);
   });
 });
