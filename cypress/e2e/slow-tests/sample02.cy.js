682f11ab0b61fc61fdc0cf4e6d592b703f8f228b
Vitor <vitorricardosilvabarbosa@gmail.com>
2022-10-29 21:34:55 -0300

    Bad habit 08 - slow tests part 4

--- cypress/e2e/slow-tests/sample02.cy.js
+++ cypress/e2e/slow-tests/sample02.cy.js
@@ -12,4 +12,24 @@ describe('Slow tests bad practice - Second sample', () => {
     cy.contains('button', 'Signup')
       .should('be.disabled');
   });
+  it('does not enable signup on different passwords', () => {
+    cy.visit('https://notes-serverless-app.com/signup');
+
+    cy.get('#email').type('joe@example.com');
+    cy.get('#password').type('foobarbaz');
+    cy.get('#confirmPassword').type('foobahbaz');
+
+    cy.contains('button', 'Signup')
+      .should('be.disabled');
+  });
+  it('does not enable signup on different passwords', () => {
+    cy.visit('https://notes-serverless-app.com/signup');
+
+    cy.get('#email').type('joe@example.com');
+    cy.get('#password').type('foobarbaz', { log: false });
+    cy.get('#confirmPassword').type('foobahbaz', { log: false });
+
+    cy.contains('button', 'Signup')
+      .should('be.disabled');
+  });
 });
